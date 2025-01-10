ab31e0d2fcc4d7c1d0de6357acb06bccd436dbe1
Saad-Driouech <saaddriouech1@gmail.com>
2025-01-10 15:13:53 +0100

    using only the first setup from exp 1

--- cls/experiments.py
+++ cls/experiments.py
@@ -115,18 +115,18 @@ def generate_experiment_cfgs(id):
     if id == 1:
         datasets = [
             ('VisDA2017', 'Synthetic', 'Real'),
-            ('OfficeHome', 'Ar', 'Cl'),
-            ('OfficeHome', 'Ar', 'Pr'),
-            ('OfficeHome', 'Ar', 'Rw'),
-            ('OfficeHome', 'Cl', 'Ar'),
-            ('OfficeHome', 'Cl', 'Pr'),
-            ('OfficeHome', 'Cl', 'Rw'),
-            ('OfficeHome', 'Pr', 'Ar'),
-            ('OfficeHome', 'Pr', 'Cl'),
-            ('OfficeHome', 'Pr', 'Rw'),
-            ('OfficeHome', 'Rw', 'Ar'),
-            ('OfficeHome', 'Rw', 'Cl'),
-            ('OfficeHome', 'Rw', 'Pr'),
+            # ('OfficeHome', 'Ar', 'Cl'),
+            # ('OfficeHome', 'Ar', 'Pr'),
+            # ('OfficeHome', 'Ar', 'Rw'),
+            # ('OfficeHome', 'Cl', 'Ar'),
+            # ('OfficeHome', 'Cl', 'Pr'),
+            # ('OfficeHome', 'Cl', 'Rw'),
+            # ('OfficeHome', 'Pr', 'Ar'),
+            # ('OfficeHome', 'Pr', 'Cl'),
+            # ('OfficeHome', 'Pr', 'Rw'),
+            # ('OfficeHome', 'Rw', 'Ar'),
+            # ('OfficeHome', 'Rw', 'Cl'),
+            # ('OfficeHome', 'Rw', 'Pr'),
         ]
         udas = [
             # uda method, alpha, weight, patch, ratio, aug
